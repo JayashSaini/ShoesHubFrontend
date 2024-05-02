2c30a07c521b7826e65623b3136f2803dbcdd204
Jayash Saini <jayashysaini7361@gmail.com>
2024-05-02 23:50:26 +0530

    refactor(Drawer): remove shadcn from drawer Component

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 });
