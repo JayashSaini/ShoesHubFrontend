99eff8be9d934b4b5c5c02525fe62215eb6e6651
Jayash Saini <jayashysaini7361@gmail.com>
2024-05-03 00:24:43 +0530

    feat(App): Implement Home section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,50 +1,16 @@
-import { PrimaryButton } from "../Components";
-import { logout } from "../features/auth";
-import { useDispatch } from "react-redux";
-import { ApiCall } from "../utils";
-// import { useNavigate } from "react-router-dom";
-
 const Home = () => {
-  const dispatch = useDispatch();
-  // const navigate = useNavigate();
-
-  const logoutHandler = async () => {
-    const accessToken = localStorage.getItem("accessToken");
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-
-    await ApiCall({
-      url: "/api/v1/users/logout",
-      method: "GET",
-      data: {},
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    dispatch(logout());
-    window.location.reload();
-  };
-
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-3xl text-blue-600 text=center mb-8">
-          Welcome to Your Dashboard
-        </h1>
-        {/* Your dashboard content goes here */}
-        <div className="flex flex-col items-center space-y-4">
-          {/* Add your dashboard content here */}
-        </div>
-        {/* Logout button */}
-        <div className="my-5 w-40">
-          <div
-            onClick={async () => {
-              await logoutHandler();
-            }}>
-            <PrimaryButton text="Log Out" />
+      <div>
+        <section className="home-bg w-full h-[85vh] flex justify-center items-end py-24 px-3">
+          <div>
+            <h2 className="text-white md:text-6xl sm:text-4xl text-5xl font-extrabold sm:text-center text-left text-shadow-lg custom-text-shadow ">
+              Comfort Meets Style. Discover <br /> Your Perfect Pair Today.
+            </h2>
           </div>
-        </div>
+        </section>
       </div>
+      <div className="w-full h-screen bg-red-500">asdf</div>
     </>
   );
 };
